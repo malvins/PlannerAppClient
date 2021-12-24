21d12dcc9f706bd8cf6fbdb205e448e74b94ed71
malvins <alvin.syahrin0795@gmail.com>
2021-12-24 14:19:33 +0700

    init httpClient instance

--- src/PlannerAppClient/Program.cs
+++ src/PlannerAppClient/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
@@ -8,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
+using System.Net.Http;
 
 namespace PlannerAppClient
 {
@@ -18,9 +19,15 @@ namespace PlannerAppClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("PlannerApp.Api", client => {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
